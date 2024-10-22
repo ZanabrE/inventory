f70930524f5348bf6222619b1de373eff4cf8a7b
Ernesto L Zanabria <ernesto.zanabria1979@gmail.com>
2024-10-21 20:43:59 -0400

    new file: inventory.cpp

---
+++ inventory.cpp
@@ -0,0 +1,8 @@
+//This program tracks the inventory of three widgets sotres
+//that opened at the same time. Each store started with the 
+//same number of widgets in inventory. By subtracting the 
+//number of widgets each store has sold from its inventory,
+//the current inventory can be calculated.
+#include <iostream>
+using namespace std;
+
